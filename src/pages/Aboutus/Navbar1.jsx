589526a132503cdc5ba9244301cdd42090d09e29
Niyasperumannil <niyaskoottilakkadavu@gmail.com>
2025-08-29 15:17:46 +0530

    29Aug1

--- src/pages/Aboutus/Navbar1.jsx
+++ src/pages/Aboutus/Navbar1.jsx
@@ -5,6 +5,11 @@ import { useNavigate } from "react-router-dom";
 // The CSS has been embedded here for a single, self-contained file.
 const embeddedCss = `
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
   :root {
     --primary-color: #080854;
